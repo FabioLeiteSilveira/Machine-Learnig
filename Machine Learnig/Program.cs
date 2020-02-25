42027c3fd1fc618bbbb93f1791bdcab99b0a8ba0
Fábio Silveira <vbasic10@hotmail.com>
2020-02-25 20:12:02 -0300

    w

---
+++ Machine Learnig/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Machine_Learnig
+{
+    using Microsoft.ML;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
